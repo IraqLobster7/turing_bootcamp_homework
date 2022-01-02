8f32fadb9bc5685a21d7e6f1ff7402edffa5afd6
Serge Barsanov <s.b.stal.invest@gmail.com>
2022-01-02 14:36:12 +0300

    исправления

--- week3/day5/path_two/js/ex5_path_two.js
+++ week3/day5/path_two/js/ex5_path_two.js
@@ -14,7 +14,8 @@ function scrollShow() {
 setInterval(scrollShow, 1);
 
 //Упражнение 3 Дата таблица с числами. По нажатию на ячейку в неё должен заноситься квадрат числа, которое она содержит
-document.getElementById('numbers').addEventListener('click', event => {
+const numbers = document.querySelector('#numbers');
+numbers.addEventListener('click', event => {
     if (event.target.className === 'numb') {
         let number = event.target;
         let square = number.innerHTML * number.innerHTML;
@@ -23,30 +24,34 @@ document.getElementById('numbers').addEventListener('click', event => {
 });
 
 //Упражнение 4 Сделайте таблицу 5x5. Используя делегирование событий окрашивать красным цветом фон той ячейки, над которой в данный момент зависает курсор
-numbers.onmouseover = function(event) {
-    let target = event.target;
-    target.style.background = 'red';
-};
-numbers.onmouseout = function(event) {
-    let target = event.target;
-    target.style.background = '';
-};
+
+numbers.addEventListener("mouseover", event => {
+    event.target.style.background = 'red';
+});
+numbers.addEventListener('mouseout', event => {
+    event.target.style.background = '';
+});
 
 //Упражнение 5 Сделать простейшую валидацию: создать инпут, при расфокусе которого будет проверяться соответствие value input’а какой-либо строке
 
-document.getElementById('blur').addEventListener('focus', event => {
+const blur = document.querySelector('#blur');
+
+blur.addEventListener('focus', event => {
     let target = event.target;
     target.style.width = '300px';
 });
-document.getElementById('blur').addEventListener('blur', event => {
+blur.addEventListener('blur', event => {
+    let testBlur = document.getElementById('test_blur');
     if (event.target.innerHTML === document.getElementById('stick').innerHTML) {
-        document.getElementById('test_blur').innerHTML = "все верно";
+        testBlur.innerHTML = "все верно";
     } else {
-        document.getElementById('test_blur').innerHTML = "Ошибочка вышла";
+        testBlur.innerHTML = "Ошибочка вышла";
     }
 });
 
 //Упражнение 6 Реализовать делегирование события фокус от элемента формы к инпутам: при фокусе граница инпута должна менять цвет и ширину
+const form = document.querySelector('#form');
+
 form.addEventListener("focusin", event => {
     let target = event.target;
     target.style.border = '3px red solid';
@@ -57,12 +62,12 @@ form.addEventListener("focusout", event => {
 });
 
 //Упражнение 7 Создайте кнопку, которая удаляет себя со страницы по нажатию на неё
-document.getElementById('delete').addEventListener('click', event => {
+document.querySelector('#delete').addEventListener('click', event => {
     event.target.parentNode.removeChild(event.target);
 });
 
 //Упражнение 8 Создайте список ol с несколькими элементами li. По нажатию на каждый элемент списка этот элемент должен удаляться
-document.getElementById('arr').addEventListener('click', event => {
+document.querySelector('#arr').addEventListener('click', event => {
     if (event.target.className === 'arr_item') { //чтобы ol не удалить
         event.target.parentNode.removeChild(event.target);
     }
